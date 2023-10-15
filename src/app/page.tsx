77883fc917bb105c6a5761d07c31554e7ee01b22
softmoca <acyc0909@gmail.com>
2023-10-15 16:13:39 +0900

    style : home Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -29,55 +29,54 @@ export default function Home() {
       </div>
       <h1 className="text-xl font-bold mb-2">학교내 소통 게시판</h1>
       <div className="mb-10  font-bold bg-gray-100 rounded-md shadow-md">
-        <div></div>
         <Link href="/jonggo">
-          <h1> 🥕 중고 거래 게시판</h1>
+          <h1 className="mb-2"> 🥕 중고 거래 게시판</h1>
         </Link>{" "}
         <Link href="/grouppurchase">
-          <h1> 📦 공동 구매 게시판</h1>
+          <h1 className="mb-2"> 📦 공동 구매 게시판</h1>
         </Link>{" "}
         <Link href="/foodfriend">
-          <h1> 🍚 밥친구 게시판</h1>
+          <h1 className="mb-2"> 🍚 밥친구 게시판</h1>
         </Link>
       </div>
       <h1 className="text-xl font-bold mb-2">핫플 건물별 소통 게시판</h1>
 
       <div className="mb-10 font-bold bg-gray-100 rounded-md shadow-md">
         <Link href="/dormitory">
-          <h1> 📌 기숙사(빛솔재)</h1>
+          <h1 className="mb-2"> 📌 기숙사(빛솔재)</h1>
         </Link>
         <Link href="/centerlibrary">
-          <h1>📌 중앙 도서관</h1>
+          <h1 className="mb-2">📌 중앙 도서관</h1>
         </Link>
         <Link href="/theater">
-          <h1>📌 노천극장</h1>
+          <h1 className="mb-2">📌 노천극장</h1>
         </Link>
         <Link href="/bokji">
-          <h1>📌 복지관</h1>
+          <h1 className="mb-2">📌 복지관</h1>
         </Link>
       </div>
       <h1 className="text-xl font-bold mb-2 ">건물별 소통 게시판</h1>
       <div className="  font-bold bg-gray-100 rounded-md shadow-md">
         <Link href="/bima">
-          <h1>📌 비마관</h1>
+          <h1 className="mb-2">📌 비마관</h1>
         </Link>{" "}
         <Link href="/chambit">
-          <h1>📌 참빛관</h1>
+          <h1 className="mb-2">📌 참빛관</h1>
         </Link>{" "}
         <Link href="/hanul">
-          <h1>📌 한울관</h1>
+          <h1 className="mb-2">📌 한울관</h1>
         </Link>{" "}
         <Link href="/saebit">
-          <h1>📌 새빛관</h1>
+          <h1 className="mb-2">📌 새빛관</h1>
         </Link>{" "}
         <Link href="/hwado">
-          <h1>📌 화도관</h1>
+          <h1 className="mb-2">📌 화도관</h1>
         </Link>{" "}
         <Link href="/okui">
-          <h1>📌 옥의관</h1>
+          <h1 className="mb-2">📌 옥의관</h1>
         </Link>
         <Link href="/nuri">
-          <h1>📌 누리관</h1>
+          <h1 className="mb-2">📌 누리관</h1>
         </Link>
       </div>
     </div>
