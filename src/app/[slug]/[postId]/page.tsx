2a06c07f6025a0fa506aaa1752e9d82a782c7403
softmoca <acyc0909@gmail.com>
2023-11-16 19:19:47 +0900

    feat : getPost Slice

--- src/app/[slug]/[postId]/page.tsx
+++ src/app/[slug]/[postId]/page.tsx
@@ -1,5 +1,25 @@
-import React from "react";
+"use client";
+
+import { getPost } from "@/redux/thunkFunctions/psotThunk";
+import { usePathname } from "next/navigation";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
 export default function page() {
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const postNamePath = usePathname();
+  const postId = postNamePath.substring(5);
+
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const dispatch = useDispatch();
+
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  useEffect(() => {
+    dispatch(getPost(postId)); //thucnk 함수 이름은 authUser
+  }, []); // 권한이 바뀌거나 or url경로가 바뀌거나
+
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const dPosts = useSelector((state) => state.persistedReducer.post.postData);
+
   return <div>개별게시물</div>;
 }
