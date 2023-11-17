3ea1676d8be289d41d32de25e5b13bf67b2a1f48
softmoca <acyc0909@gmail.com>
2023-11-17 20:18:48 +0900

    feat : getComments

---
+++ src/redux/thunkFunctions/commentThunk.ts
@@ -0,0 +1,17 @@
+import axiosInstance from "@/utils/axios";
+import { createAsyncThunk } from "@reduxjs/toolkit";
+
+export const getComments = createAsyncThunk(
+  "post/getComments",
+  async (postId: string, thunkAPI) => {
+    try {
+      const response = await axiosInstance.get(`post/${postId}/comment`);
+
+      //console.log(response.data.data);
+      return response.data.data;
+    } catch (error) {
+      console.log(error);
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
