2b1428067e5c510ee2ee78f4c7b6afc0b774ee11
softmoca <acyc0909@gmail.com>
2023-11-27 21:06:12 +0900

    refactory : signup

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,21 +1,57 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable @next/next/no-img-element */
 "use client";
 
 import { registerUser } from "@/redux/thunkFunctions/userThunk";
 import Link from "next/link";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 import { useDispatch } from "react-redux";
+import { toast } from "react-toastify";
+import axios from "axios";
+import { useRouter } from "next/navigation";
 
 export default function RegisterPage() {
+  let router = useRouter();
+  const dispatch = useDispatch();
+  const [emailPass, setEmailPass] = useState(false);
+  const [nickNamePass, setNickNamePass] = useState(false);
+
   const {
     register,
     handleSubmit,
     formState: { errors },
     reset,
-  } = useForm({ mode: "onChange" });
+    watch,
+    getValues,
+    setError,
+    clearErrors,
+  } = useForm({
+    mode: "onChange",
+    defaultValues: {
+      email: "",
+      nickName: "",
+      password: "",
+      passwordCheck: "",
 
-  const dispatch = useDispatch();
+      term: false,
+    },
+  });
+
+  useEffect(() => {
+    if (
+      watch("password") !== watch("passwordCheck") &&
+      watch("passwordCheck")
+    ) {
+      setError("passwordCheck", {
+        type: "password-mismatch",
+        message: "비밀번호가 일치하지 않습니다",
+      });
+    } else {
+      // 비밀번호 일치시 오류 제거
+      clearErrors("passwordCheck");
+    }
+  }, [watch("password"), watch("passwordCheck")]);
 
   const onSubmit = ({ email, password, nickName, university }) => {
     // 페이지에서 입력 한 값
@@ -26,8 +62,59 @@ export default function RegisterPage() {
       university: university,
     };
 
-    dispatch(registerUser(body));
-    reset(); //react-hook-form으로 입력후 입력값 초기화
+    if (emailPass && nickNamePass) {
+      dispatch(registerUser(body));
+      // reset(); //react-hook-form으로 입력후 입력값 초기화
+      console.log("dddd");
+      setTimeout(() => {
+        console.log("aaaaaaaa");
+        router.push("/signin");
+      }, 500);
+      console.log("dddd");
+      console.log("dddsdfdsd");
+    } else {
+      toast.error(" 중복 체크들를 먼저 해주세요 !");
+    }
+
+    // reset(); //react-hook-form으로 입력후 입력값 초기화
+  };
+
+  const emailCheck = async () => {
+    const emailValue = watch("email");
+    console.log(emailValue);
+    try {
+      const response = await axios.post(
+        "http://localhost:3030/user/checkEmail", //백엔드 api url
+        { email: emailValue }
+      );
+
+      if (response.data.success) {
+        setEmailPass(true);
+        toast.info("이메일 사용 가능합니다 ! !");
+      }
+    } catch (error) {
+      setEmailPass(false);
+      toast.error(error.response.data.message[0]);
+    }
+  };
+
+  const nickNameCheck = async () => {
+    const nickNameValue = watch("nickName");
+    console.log(nickNameValue);
+    try {
+      const response = await axios.post(
+        "http://localhost:3030/user/checkNickName", //백엔드 api url
+        { nickName: nickNameValue }
+      );
+
+      if (response.data.success) {
+        setNickNamePass(true);
+        toast.info("닉네임 사용 가능합니다 ! !");
+      }
+    } catch (error) {
+      setNickNamePass(false);
+      toast.error(error.response.data.message[0]);
+    }
   };
 
   const userEmail = {
@@ -36,21 +123,31 @@ export default function RegisterPage() {
   const usernickName = {
     required: "필수 필드입니다.",
   };
+
   const userPassword = {
     required: "필수 필드입니다.",
-    minLength: {
-      value: 6,
-      message: "최소 6자입니다.",
+    pattern: {
+      value: /^(?=.*[a-zA-Z])(?=.*[0-9]).{4,}$/,
+      message: "영어와 숫자 조합으로 4글자 이상으로 입력해주세요",
     },
   };
+
+  const userPasswordCheck = {
+    required: "필수 필드입니다.",
+    validate: {
+      matchPassword: (value) => {
+        const { password } = getValues();
+        return password === value || "비밀번호가 일치하지 않습니다";
+      },
+    },
+  };
+
   const useruniversity = {
     required: "필수 필드입니다.",
   };
 
   return (
-    // flex-col 자식요소 수직 , max-w-[400px] 최대 너비 제한   m-auto 좌우 여백을 자동으로 설정
-
-    <section className="flex flex-col justify-center mt-5 max-w-[400px] m-auto">
+    <section className="flex flex-col justify-center m-3  max-w-[400px]">
       <div className=" bg-white rounded-md board">
         <img
           className="mx-auto my-2 w-40 h-15"
@@ -80,7 +177,7 @@ export default function RegisterPage() {
           >
             <option value="광운대학교">광운대학교</option>
             <option value="영철대학교">영철대학교</option>
-            <option value="모카대학교">모카대학교</option>
+            <option value="호그와트">호그와트</option>
             <option value="서울대학교">서울대학교</option>
             <option value="연세대학교">연세대학교</option>
             <option value="고려대학교">고려대학교</option>
@@ -92,18 +189,33 @@ export default function RegisterPage() {
             <label htmlFor="name" className="font-semibold  text-gray-800">
               닉네임
             </label>
-            <input
-              placeholder="닉네임을 입력해주세요."
-              type="nickName"
-              id="nickName"
-              className="w-full px-4 py-2 mt-1 border bg-white rounded-md"
-              {...register("nickName", usernickName)}
-            ></input>
-            {errors?.name && (
+            <div className="flex item-center">
+              {" "}
               <div>
-                <span className="text-red-500">{errors.name.message}</span>
+                {" "}
+                <input
+                  placeholder="닉네임을 입력해주세요."
+                  type="nickName"
+                  id="nickName"
+                  className=" px-4 py-2 mt-1 text-sm border bg-white rounded-md mr-2 flex-grow"
+                  {...register("nickName", usernickName)}
+                ></input>
               </div>
-            )}
+              <button
+                type="button"
+                onClick={nickNameCheck}
+                className=" px-4 py-2 mt-1 text-white duration-200 text-sm bg-black rounded-md  hover:bg-gray-700 "
+              >
+                중복 체크
+              </button>{" "}
+            </div>
+            {errors?.nickName && (
+              <div>
+                <span className="text-red-500 px-2 py-2 mt-1 text-sm  ">
+                  {errors.nickName.message}
+                </span>
+              </div>
+            )}{" "}
           </div>
 
           <div className="mb-3">
@@ -113,34 +225,76 @@ export default function RegisterPage() {
             >
               이메일
             </label>
-            <input
-              placeholder="이메일을 입력해주세요."
-              type="email"
-              id="email"
-              className="w-full px-4 py-2  mt-1 border bg-white rounded-md" //px 외부 여백 py 내부 여백 상하좌우
-              {...register("email", userEmail)} //register() 함수의 반환값은 객체이기 때문에 ... 사용 첫번째 인자는 type
-            ></input>
+
+            <div className="flex items-center">
+              {" "}
+              <div>
+                {" "}
+                <input
+                  placeholder="이메일을 입력해주세요."
+                  type="email"
+                  id="email"
+                  className=" px-4 py-2   text-sm mt-1 border bg-white mr-2 rounded-md flex-grow" //px 외부 여백 py 내부 여백 상하좌우
+                  {...register("email", userEmail)} //register() 함수의 반환값은 객체이기 때문에 ... 사용 첫번째 인자는 type
+                ></input>
+              </div>
+              <button
+                type="button"
+                onClick={emailCheck}
+                className=" px-4 py-2 mt-1 text-sm text-white duration-200 bg-black rounded-md  hover:bg-gray-700 "
+              >
+                중복 체크
+              </button>
+            </div>
+
             {errors?.email && (
               <div>
-                <span className="text-red-500">{errors.email.message}</span>
+                <span className="text-red-500 px-2 py-2 mt-1 text-sm">
+                  {errors.email.message}
+                </span>
               </div>
             )}
           </div>
 
           <div className="mb-3">
             <label htmlFor="password" className="font-semibold text-gray-800">
-              비밀번호
+              PW1(비밀번호)
             </label>
             <input
-              placeholder="비밀번호를 입력해주세요."
+              placeholder="영어와 숫자 조합으로 4글자 이상으로 입력해주세요."
               type="password"
               id="pasword"
-              className="w-full px-4 py-2 mt-1 border bg-white rounded-md"
+              className="w-full px-4 py-2 mt-1 border text-sm bg-white rounded-md"
               {...register("password", userPassword)}
             ></input>
             {errors?.password && ( //? 는 옵셔널 체크 연산자, password 라는 속성이 없으면 undefined
               <div>
-                <span className="text-red-500">{errors.password.message}</span>
+                <span className="text-red-500 px-2 py-2 mt-1 text-sm">
+                  {errors.password.message}
+                </span>
+              </div>
+            )}
+          </div>
+
+          <div>
+            <label
+              htmlFor="passwordcheck"
+              className="text-sm font-semibold text-gray-800"
+            >
+              PW2(비밀번호 확인)
+            </label>
+            <input
+              placeholder="비밀번호 확인"
+              type="passwordcheck"
+              id="passwordcheck"
+              className="w-full px-4 py-2  text-sm mt-2 border bg-white rounded-md"
+              {...register("passwordCheck", userPasswordCheck)}
+            ></input>
+            {errors?.passwordCheck && ( //? 는 옵셔널 체크 연산자, password 라는 속성이 없으면 undefined
+              <div>
+                <span className="text-red-500 px-2 py-2 mt-1 text-sm">
+                  {errors.passwordCheck.message}
+                </span>
               </div>
             )}
           </div>
